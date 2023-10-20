1b405501c676720da4f6ba768690518b1b4e1975
Weng Hong :D <bobosgagas@hotmail.com>
2023-10-21 01:30:21 +0800

    logout navigation

--- frontend/AppNav.js
+++ frontend/AppNav.js
@@ -141,13 +141,16 @@ export default function AppNav() {
   function FifthTabStack() {
     return (
       <Stack.Navigator>
-        <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+        <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false } } initialParams={{ setAuthenticated: setAuthenticated }}  />
           <Stack.Screen name="Language" component={Language} />
           <Stack.Screen name="Achievement" component={Achievement} />
           <Stack.Screen name="Shop" component={Shop} />
           <Stack.Screen name="Help Centre" component={FAQScreen} />
           <Stack.Screen name="Contact Us" component={ContactUsScreen} />
-          <Stack.Screen name="Report A Bug" component={ReportIssue} />
+        <Stack.Screen name="Report A Bug" component={ReportIssue} />
+        
+        
+        
         {/* Add more screens as needed within the FirstTab */}
       </Stack.Navigator>
     );
