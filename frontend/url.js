2ec31646d7c0079da213e2befd3b908654d07fb5
Weng Hong :D <bobosgagas@hotmail.com>
2023-10-22 02:57:52 +0800

    update

--- frontend/url.js
+++ frontend/url.js
@@ -1,2 +1,3 @@
 // export default url = "http://172.20.10.13:3000";
-export default url = "http://10.164.106.217:3000";
+export default url = "http://192.168.0.5:3000";
+// export default url = "http://10.164.106.217:3000";
